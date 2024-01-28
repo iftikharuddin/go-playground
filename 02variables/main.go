f735056eb1735bdb67af5dcc68c4c96c49bca04f
Iftikhar uddin <iftipro@gmail.com>
2024-01-28 19:16:47 +0500

    Variables, types and constants

---
+++ 02variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Variables")
+
+    // implicit type
+    var website = "testing.com"
+    fmt.Println(website)
+
+    // no var style
+    numberOfUser := 30000.0
+    fmt.Println(numberOfUser)
+
+}
